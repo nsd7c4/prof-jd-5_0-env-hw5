fa2920184198966051112cba06e2f1fc53815ca8
rashidgafurov <nsd7c4@gmail.com>
2022-05-03 11:01:53 -0400

    Initial commit

---
+++ src/test/java/com/rginc/profjd5_0envhw5/ProfJd50EnvHw5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rginc.profjd5_0envhw5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfJd50EnvHw5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
